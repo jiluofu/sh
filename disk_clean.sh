830e65166112540770fef401d72d7c50ac504726
zhuxu <zhuxu@baidu.com>
2018-02-01 10:58:20 +0800

    =update

---
+++ disk_clean.sh
@@ -0,0 +1,4 @@
+rm -rf ~/Library/Application\ Support/MobileSync/Backup/*
+rm -rf ~/Music/iTunes/iTunes\ Media/Music/*
+rm -rf ~/Music/iTunes/iTunes\ Media/Podcasts/*
+rm -rf ~/Music/iTunes/iTunes\ Media/Voice\ Memos/*
