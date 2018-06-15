05e47b7779d4d4717d85068890530f0a815dce93
zhuxu <zhuxu@baidu.com>
2018-06-15 16:45:30 +0800

    =update

---
+++ backup.sh
@@ -0,0 +1,6 @@
+target_dir="/Volumes/U64/"
+workDoc_dir="/Users/zhuxu/Documents/bak/workDoc"
+cp -rf $workDoc_dir $target_dir
+
+optDoc_dir="/Users/zhuxu/Documents/bak/optDoc"
+cp -rf $optDoc_dir $target_dir
