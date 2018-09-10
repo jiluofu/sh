985e524f2b9df760850b6df8e287b7e510f909f3
zhuxu <zhuxu@baidu.com>
2018-09-10 10:31:00 +0800

    =update

--- disk_clean.sh
+++ disk_clean.sh
@@ -2,3 +2,4 @@ rm -rf ~/Library/Application\ Support/MobileSync/Backup/*
 rm -rf ~/Music/iTunes/iTunes\ Media/Music/*
 rm -rf ~/Music/iTunes/iTunes\ Media/Podcasts/*
 rm -rf ~/Music/iTunes/iTunes\ Media/Voice\ Memos/*
+rm -rf ~/Library/Caches/com.apple.dt.Xcode/Downloads/*
