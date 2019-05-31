190bc963267730d31729346cb043aa2ae1c681f9
zhuxu <zhuxu@baidu.com>
2019-05-31 10:04:44 +0800

    =minus

--- py_math.py
+++ py_math.py
@@ -4,16 +4,31 @@ import random
 
 def minus_2(a1, a2):
     
-    str = '{} - {} = (  )'.format(a1, a2)
+    str = '{} - (   ) = {}'.format(a1, a2)
     return str
 
-def gen_q():
-    
+def get_a1():
+
     a1 = random.randrange(20, 90)
     bit_a1 = a1 % 10
     dec_a1 = int(a1 % 100 / 10)
+    return {
+        'a1' : a1, 
+        'bit_a1': bit_a1,
+        'dec_a1': dec_a1
+    }
+
+def gen_q():
     
-    if bit_a1 == 0 or bit_a1 == 9:
+    dict_a1 = get_a1()
+    while dict_a1['bit_a1'] == 9:
+        dict_a1 = get_a1()
+
+    a1 = dict_a1['a1']
+    bit_a1 = dict_a1['bit_a1']
+    dec_a1 = dict_a1['dec_a1']
+    
+    if bit_a1 == 0:
         bit_a2 = random.randrange(bit_a1, 10)
     else:
         bit_a2 = random.randrange(bit_a1 + 1, 10)
@@ -27,13 +42,17 @@ def gen_q():
     return minus_2(a1, a2)
 
 def render():
-
-    for i in range(20):
-        print(gen_q())    
+    lines = []
+    for i in range(132):
+        lines.append(gen_q())
+    random.shuffle(lines)  
+    
+    return lines  
 
 if __name__ == '__main__':
     
-    render()
+    lines = render()
+    print('\n\n'.join(lines))
     
 
 
