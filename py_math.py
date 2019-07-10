6859cd6602561afc48b2120d5e5a9c3a5e1837da
zhuxu <zhuxu@baidu.com>
2019-07-11 06:04:18 +0800

    =update money_minus

--- py_math.py
+++ py_math.py
@@ -1,11 +1,37 @@
 #!/usr/local/bin/python3
 
+import sys
 import random
+
+runType = 'minus'
+if len(sys.argv) > 1:
+    runType = sys.argv[1]
 
 def minus_2(a1, a2):
     
     str = '{} - (   ) = {}'.format(a1, a2)
     return str
+
+def money_minus(a1, a2):
+
+    price_a1 = get_price(a1, random.randrange(30, 60))
+    price_a2 = get_price(a2, random.randrange(10, 20))
+
+    products = ['冰棍', '蒙奇奇', '钢铁侠', '美国队长', '黑寡妇', '红女巫']
+    
+    str = '{}价格{}，你付了{}，老板找你（  ）元（  ）角'.format(products[random.randrange(0, len(products) - 1)], price_a2, price_a1)
+    return str
+
+def get_price(a, do_add):
+    bit_a = a % 10
+    dec_a = int(a % 100 / 10)
+    dec_a = dec_a + do_add
+    price_a = ''
+    if bit_a > 0:
+        price_a = '{}角'.format(bit_a)
+    if dec_a > 0:
+        price_a = '{}元{}'.format(dec_a, price_a)
+    return price_a
 
 def get_a1():
 
@@ -39,11 +65,14 @@ def gen_q():
         dec_a2 = random.randrange(0, dec_a1 - 1)
     a2 = dec_a2 * 10 + bit_a2
 
-    return minus_2(a1, a2)
+    if runType == 'minus':
+        return minus_2(a1, a2)
+    elif runType == 'money_minus':
+        return money_minus(a1, a2)
 
 def render():
     lines = []
-    for i in range(132):
+    for i in range(30):
         lines.append(gen_q())
     random.shuffle(lines)  
     
