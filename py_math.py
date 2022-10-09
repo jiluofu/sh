9f6be8db3c1b66428758ad902bc11be956a6c0d8
jiluofu <zhuxu@baidu.com>
2022-10-10 06:13:22 +0800

    =update

--- py_math.py
+++ py_math.py
@@ -38,6 +38,9 @@ def get_a1():
     a1 = random.randrange(20, 90)
     bit_a1 = a1 % 10
     dec_a1 = int(a1 % 100 / 10)
+    print(a1)
+    print(bit_a1)
+    print(dec_a1)
     return {
         'a1' : a1, 
         'bit_a1': bit_a1,
@@ -102,11 +105,76 @@ def gen_mul():
     # elif runType == 'money_minus':
     #     return money_minus(a1, a2)
 
+def get_a1120():
+
+    a1 = random.randrange(11, 30)
+    return a1
+
+def get_a110(a1):
+
+    a2 = random.randrange(1, a1)
+    while judge_a1120(a1, a2) == False:
+        # a1 = get_a1120()
+        a2 = random.randrange(1, a1)
+    return a2
+def judge_a1120(a1, a2):
+    bit_a1 = a1 % 10
+    bit_a2 = a2 % 10
+    # if a1 <= a2:
+    #     return False
+    if a1 > 10 and a2 < 10 and bit_a1 >= a2:
+        return False
+    if a1 > 10 and a2 > 10 and (bit_a1 > bit_a2) and bit_a1 != 9:
+        print(a1)
+        print(a2)
+        print(bit_a1)
+        print(bit_a2)
+        print('22222222')
+        return False
+
+    return True
+
+def minus_3(a1, a2):
+    
+    str = '{} - {} = '.format(a1, a2)
+    return str
+
+def gen_q20():
+    
+    a1 = get_a1120()
+    a2 = get_a110(a1)
+    while a2 == 10:
+        a2 = get_a110(a1)
+    return minus_3(a1, a2)
+
+def get_a9(a1):
+
+    a2 = random.randrange(1, 9)
+    
+    while a1 == a2 or a2 == 1:
+        a2 = random.randrange(1, 9)
+    return a2
+
+def mul_9(a1, a2):
+    
+    str = '{} × {} = '.format(a1, a2)
+    return str
+
+def gen_m9():
+    
+    a1 = random.randrange(2, 9)
+    a2 = get_a9(a1)
+    return mul_9(a1, a2)
+
 def render():
     lines = []
-    for i in range(33):
+    for i in range(66):
         # lines.append(gen_q())
-        lines.append(gen_mul())
+        # lines.append(gen_mul())
+        if i % 2 == 0:
+            lines.append(gen_q20())
+        else:
+            lines.append(gen_m9())
     random.shuffle(lines)  
     
     return lines  
