0610faa7800552e4bddf51d03263f6551ad1d877
jiluofu <zhuxu@baidu.com>
2022-11-23 14:56:27 +0800

    =update

--- shell_noise.sh
+++ shell_noise.sh
@@ -2,6 +2,7 @@
 # 删除输出目录，重建
 rm -rf $1/output
 mkdir $1/output
+mkdir $1/output1
 
 for i in $1/*.WAV
 do
@@ -9,6 +10,7 @@ ffmpeg -y -i $i -ss 00:00:00.70 -t 00:00:01.10 noiseSample.wav
 
 sox $1/noiseSample.wav -n noiseprof noisepf.prof
 sox $i $1/output/$i noisered noisepf.prof 0.21
+ffmpeg -y -i $1/output/$i -af volume=5dB $1/output1/$i
 rm -rf noise*.*
 done
 
