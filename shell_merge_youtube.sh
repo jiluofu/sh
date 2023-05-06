5954200741a73e3615feedd20e4f04094f43b049
zhuxu <zhuxu@jiluofu.local>
2023-05-06 14:13:25 +0800

    =xima

--- shell_merge_youtube.sh
+++ shell_merge_youtube.sh
@@ -10,13 +10,13 @@ do
 # ffmpeg -y -r $fps -i $i  -vcodec mpeg4 -s $width*$height -b:v $bv output/$i
 # ffmpeg -i $i -c copy -c:v libx264 -vf scale=-2:720 output/$i
 # echo `ffmpeg -i $1.mp4 -f mp3 -vn $1.mp3`
-# ffmpeg -i $i -f mp3 -vn ${i/mp4/mp3}
+# ffmpeg -i $i -f mp3 -vn ${i/mp4/mp3}	
 ffmpeg -i $i -vn -codec copy ${i/mp4/m4a}
 done
 
 
 
-ffmpeg -i "videoplayback.webm" -i "videoplayback.m4a" -vcodec copy -acodec copy $1/output/all.mp4
+ffmpeg -i "videoplayback.mp4" -i "videoplayback.m4a" -vcodec copy -acodec copy $1/output/all.mp4
 
 
 
