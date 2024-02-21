e11a3712edca5a91d94fbc0a91d4a4e19e2ed0b2
jiluofu <zhuxu@jiluofu.local>
2024-02-21 15:12:43 +0800

    =update

--- shell_merge_youtube.sh
+++ shell_merge_youtube.sh
@@ -1,6 +1,29 @@
+#!/bin/bash
 
-ffmpeg -i $1/videoplayback.mp4 -vn -codec copy $1/videoplayback.m4a
-ffmpeg -i "videoplayback.webm" -i "videoplayback.m4a" -vcodec copy -acodec copy $1/videoplaybackall.mp4
+# ffmpeg -i $1/videoplayback.mp4 -vn -codec copy $1/videoplayback.m4a
+
+# ffmpeg -i "videoplayback.mp4" -i "videoplayback.m4a" -vcodec copy -acodec copy $1/videoplaybackall.mp4
+
+# for f in *;
+# do echo "file '$PWD/$f'";echo "name '$f'";
+
+
+# # 删除输出目录，重建
+# rm -rf $PWD/$f/output
+# mkdir $PWD/$f/output
+# ffmpeg -y -f concat -safe 0 -i <(for ff in $PWD/$f/*.m4a; do echo "file '$ff'"; done) -c copy $PWD/$f/output/$f.m4a
+
+# done
+
+# 删除输出目录，重建
+rm -rf $1/output
+mkdir $1/output
+
+for f in *.webm;
+do prefix="${f%.*}";echo "$prefix";echo "video '$f'";
+# ffmpeg -i $f -i $prefix.mp4 -c:v hevc_videotoolbox -preset medium -acodec copy $1/output/$prefix.mp4;
+ffmpeg -i $f -i $prefix.mp4 -vcodec copy -acodec copy $1/output/$prefix.mp4;
+done
 
 
 
