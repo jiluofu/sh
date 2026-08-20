dbc849efdfec7ae75b3b6e862fa5fd87f467d755
jiluofu <jiluofu@gmail.com>
2026-08-20 16:02:59 +0800

    =update weather

--- shell_down_weather.sh
+++ shell_down_weather.sh
@@ -7,43 +7,43 @@ else
 time=$1
 fi
 
-output="/Users/zhuxu/Downloads"
-# output="/home/zhuxu/mmjs_server/gohttps_loader/static/weather"
+# output="/Users/zhuxu/Downloads"
+output="/home/zhuxu/mmjs_server/gohttps_loader/static/weather"
 
-# # 下载直播
-# # ffmpeg -y -i http://audiolive.rbc.cn:1935/live/fm1039/96K/tzwj_video.m3u8 -ss 0 -to 120  test.mp4
-# url="http://audiolive.rbc.cn:1935/live/fm1039/96K/tzwj_video.m3u8"
-# name=$time"_ylct.mp4"
-# echo $url
-# echo $name
-# # wget -O $output/$name $url
-# weather_name=$time"_weather.mp4"
-# echo $weather_name
-# ffmpeg -y -i $url -ss 00:00:00 -to 00:08:30 $output/$weather_name
-
-
-# 下载回放
-url="http://playback.rbc.cn/audioD/fm1039/$time/0730007200_mp4/073000_7200_96K.mp4"
+# 下载直播
+# ffmpeg -y -i http://audiolive.rbc.cn:1935/live/fm1039/96K/tzwj_video.m3u8 -ss 0 -to 120  test.mp4
+#url="https://rtmp-push.tingtingfm.com/bjradio/fm1039.m3u8?auth_key=1774672183-ydWDv4YW-0-469988d52317238ed776c06499eae619"
+url="https://brtv-radiolive.rbc.cn/alive/fm1039.m3u8?auth=2346235872047145@36@288EA47F2FB2233054F8C6A94FF01F6E"
 name=$time"_ylct.mp4"
 echo $url
 echo $name
-wget -O $output/$name $url
+# wget -O $output/$name $url
 weather_name=$time"_weather.mp4"
 echo $weather_name
-ffmpeg -y -i $output/$name -ss 00:27:00 -to 00:42:30 -acodec copy $output/$weather_name
+ffmpeg -y -i $url -ss 00:00:00 -to 00:12:30   $output/$weather_name
+
+
+# # 下载回放
+# url="http://playback.rbc.cn/audioD/fm1039/$time/0730007200_mp4/073000_7200_96K.mp4"
+# name=$time"_ylct.mp4"
+# echo $url
+# echo $name
+# wget -O $output/$name $url
+# weather_name=$time"_weather.mp4"
+# echo $weather_name
+# ffmpeg -y -i $output/$name -ss 00:31:00 -to 00:38:30 -acodec copy $output/$weather_name
 
 
 rm -rf $output/$name
-weather_file_name=$time"_专家聊天气.mp3"
-weather_file_name_wav=$time"_专家聊天气.wav"
+weather_file_name=$time"_weather.mp3"
+weather_file_name_wav=$time"_weather.wav"
 ffmpeg -y -i $output/$weather_name  -f mp3 -vn $output/$weather_file_name
-ffmpeg -y -i $output/$weather_name  -f wav $output/$weather_file_name_wav
-rm -rf $output/$weather_name
+ffmpeg -y -i $output/$weather_name  -ac 1 -ar 16000 $output/$weather_file_name_wav
+#rm -rf $output/$weather_name
 
-# echo $time"_专家聊天气" | mailx -s $time"_天气和专家" -a $output/$weather_file_name  1077246@qq.com
-# ./py_weather.py $time
+/home/zhuxu/mmjs_server/sh/py_weather_serial.py $time
+echo $time"_weather" | s-nail -s $time"_weather" -a $output/output/$weather_file_name  1077246@qq.com
 
  
-
 
 
